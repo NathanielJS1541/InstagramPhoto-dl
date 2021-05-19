9a01dc39744594fd6e3375f1dd7cbd3587a9bc00
Nathaniel Struselis <Nathaniel.Struselis@protonmail.com>
2021-05-19 11:24:37 +0100

    Added basic try except statement to catch errors where the link only contains one photo.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ The purpose of this python script is to offer a method to download photos from i
 with an URL similar to the way that `youtube-dl` works. This is intended to be installed in
 Termux on a phone with a custom `termux-url-opener` script to save instagram photos.
 
-##**THIS IS A WORK IN PROGRESS**
+## **THIS IS A WORK IN PROGRESS**
--- instagram-dl.py
+++ instagram-dl.py
@@ -7,7 +7,8 @@ import wget
 outPath = "Instagram/"
 
 # Target URL
-instaURL = 'https://www.instagram.com/p/CHBGznoLvlEHd_ndv4YD0Qj9dGZtr67XLHk6vI0/?__a=1'
+instaURL = 'https://www.instagram.com/p/CPB9WEwh-G8/'
+instaURL = instaURL + '?__a=1'  # Update the URL to get the JSON manifest
 
 # Import cookies to avoid the need to login
 cookiesFile = http.cookiejar.MozillaCookieJar("instagram.com_cookies.txt")
@@ -22,17 +23,23 @@ status = requests.get(instaURL, cookies=cookiesFile).status_code
 # Extract picture URLs
 focusURL = jsonDict['graphql']['shortcode_media']['display_resources']
 focusURL = focusURL[len(focusURL)-1]['src']     # The last entry in the JSON will be the highest resolution
-childrenURL = jsonDict['graphql']['shortcode_media']['edge_sidecar_to_children']['edges']
-noOfChildren = len(childrenURL)
-uniqueURLs = [focusURL]
-# Cycle through children URLs (Not all posts have these, how do we handle this?)
-for i in range(noOfChildren):
-    if childrenURL[i]['node']['display_url'] not in uniqueURLs:
-        uniqueURLs.append(childrenURL[i]['node']['display_url'])
+
+# Check for multiple photos in the same post
+try:
+    childrenURL = jsonDict['graphql']['shortcode_media']['edge_sidecar_to_children']['edges']
+    print("[INFO]: Multiple Photos Found: Downloading All.")
+    noOfChildren = len(childrenURL)
+    uniqueURLs = [focusURL]
+    # Cycle through children URLs (Not all posts have these, how do we handle this?)
+    for i in range(noOfChildren):
+        if childrenURL[i]['node']['display_url'] not in uniqueURLs:
+            uniqueURLs.append(childrenURL[i]['node']['display_url'])
+except KeyError:
+    print("[INFO]: Only One Photo Found. Downloading.")
+    uniqueURLs = [focusURL]
 
 # Download the pictures from the URLs
 for i in range(len(uniqueURLs)):
     wget.download(uniqueURLs[i], out=outPath)
 
 print("Status:", status)
-print(uniqueURLs)
